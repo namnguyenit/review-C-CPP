a180dba26e546731af46770d83f30ac062bf3291
Cao Đức Trung <97675663+namnguyenit@users.noreply.github.com>
2026-07-01 15:40:21 +0700

    up

--- test.c
+++ test.c
@@ -1,30 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+struct Node {
+  int data;
 
-struct node {
-    int data;
-    struct node *next;
+  struct Node *next;
+};
+
+struct Node *newnode(int data){
+  struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+  temp->data=data;
+
+  temp->next=NULL;
+  return temp;
+
 }
 
-struct node *newnode(int data){
-    struct node *temp = (struct Node*)malloc(sizeof(struct Node));
-    temp->data = data;
-    temp->node = NULL;
-    return *temp;
-}
+void themnode(struct Node **head,int data){
+  struct Node *temp = newnode(data);
 
-struct node *themnode(struct node **head, int data){
-    struct node *temp = newnode(data);
-    if (*head == NULL ){
-        temp->next =  temp;
-        *head = temp;
-    }else{
-        struct node *t = *head;
-        while (t->next != temp)
+  if(*head == NULL){
+    temp->next=temp;
+    *head = temp; 
+  }else{
+    struct Node *t = *head;
+    while(t->next != *head){
+      t=t->next;
     }
+    t->next=temp;
+    temp->next=*head;
+  }
+}
+void duyett(struct Node *head){
+    if(head == NULL) return ;
+    struct Node *temp = head;
+    struct Node *next_node;
+    do{
+        next_node = temp->next;
+        printf("%d ", temp->data);
+        temp = next_node;
+    }while (temp != head);
+}
+void Free(struct Node *head) {
+    if (head == NULL) return;  
 
+    struct Node *temp = head;
+    struct Node *next_node;
+
+    
+    do {
+        next_node = temp->next;
+        free(temp);
+        temp = next_node;
+    } while (temp != head);
 }
 
 int main(){
-    printf("Test\n");
+  int n;
+  scanf("%d" , &n);
+  struct Node *head = NULL;
+    for(int i =0 ; i< n;i++){
+      int k;
+      scanf("%d" , &k);
+      themnode(&head,k);
+    }
+      duyett(head);
+    Free(head);
 }
