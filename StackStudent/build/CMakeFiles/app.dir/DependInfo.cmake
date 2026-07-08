a0e6112b35d741072a16f6a8974533661926bcc1
Cao Đức Trung <97675663+namnguyenit@users.noreply.github.com>
2026-07-08 15:27:36 +0700

    Cmake

---
+++ StackStudent/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/trungcao/training/review-C/linklist/src/linklist.c" "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/linklist.c.o" "gcc" "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/linklist.c.o.d"
+  "/home/trungcao/training/review-C/linklist/src/student.c" "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/student.c.o" "gcc" "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/student.c.o.d"
+  "/home/trungcao/training/review-C/StackStudent/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  "/home/trungcao/training/review-C/StackStudent/src/StackStudent.cpp" "CMakeFiles/app.dir/src/StackStudent.cpp.o" "gcc" "CMakeFiles/app.dir/src/StackStudent.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ StackStudent/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/linklist.c.o"
+  "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/linklist.c.o.d"
+  "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/student.c.o"
+  "CMakeFiles/app.dir/home/trungcao/training/review-C/linklist/src/student.c.o.d"
+  "CMakeFiles/app.dir/main.cpp.o"
+  "CMakeFiles/app.dir/main.cpp.o.d"
+  "CMakeFiles/app.dir/src/StackStudent.cpp.o"
+  "CMakeFiles/app.dir/src/StackStudent.cpp.o.d"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
