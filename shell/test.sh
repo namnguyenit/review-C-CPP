68787e34ced03f4db37c266ba350bb16f0e9ae05
Cao Đức Trung <97675663+namnguyenit@users.noreply.github.com>
2026-07-09 15:27:07 +0700

    up

---
+++ shell/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# NAME="trung"
+# echo $NAME
+# echo "Hello, $NAME!"
+# echo "$NAME"
+# echo '$NAME'
+
+# DATE=$(date)
+
+# echo $DATE
+
+# LS=$(ls)
+# echo $LS
+
+# echo "Nhap ten: "
+# read TEN
+# echo "xin chao $TEN"
+
+# read -p "Nhap ten: " NAME
+
+# echo $NAME
+
+readonly PI=3.14
+
+unset PI
+
+echo PI
+
